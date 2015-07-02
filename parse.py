e69a93bb487b361beb6bafd5edbfbe96795e0687
KirillUdod <kirilludod@gmail.com>
2015-07-02 19:31:35 +0300

    adding forms

--- parse.py
+++ parse.py
@@ -1,15 +1,29 @@
-import requests  
+import sys
+import requests
 from lxml import html
+from PyQt5.QtWidgets import QMainWindow, QLabel, QComboBox, QApplication, QPushButton, QTextEdit
 
 
-HREFS = {
-
+_SECTIONS_REF = {
+    'За сутки': '/top/',
+    'За неделю': '/top/weekly/',
+    'За месяц': '/top/monthly/',
+    'За все время': '/top/alltime/',
 }
+
+_FORMAT = {
+    'JSON',
+    'YAML',
+}
+
+SITE_REF = 'http://habrahabr.ru'
+
 
 class Parser(object):
 
-    def __init__(self):
-        self.site_refer = 'http://habrahabr.ru'
+    def __init__(self, page_ref):
+        self.site_refer = SITE_REF
+        self.parse_site(page_ref)
 
     def parse_site(self, page_ref):
         refer = self.site_refer + page_ref
@@ -19,8 +33,8 @@ class Parser(object):
             for post in parsed_body.cssselect('div.post'):
                 hubs = ''
                 for post_title in post.cssselect('a.post_title'):
-                    print(post_title.text_content())
-                    print(post_title.get('href'))
+                    post_title.text_content()
+                    post_title.get('href')
                 for hub in post.cssselect('a.hub'):
                     hubs = hubs + hub.text_content() + ', '
                 print(hubs[:-2])
@@ -42,7 +56,95 @@ class Parser(object):
                     refer = self.site_refer + i.get('href')
 
 
+class MenuWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.href = '/top/'
+        self.format = 'JSON'
+        self.window2 = None
+        self.create_main_window()
+
+    def create_main_window(self):
+
+        lbl1 = QLabel("Choose needed section", self)
+        lbl1.setGeometry(40, 0, 200, 35)
+
+        self.sections_combo = QComboBox(self)
+        for ref in _SECTIONS_REF:
+            self.sections_combo.addItem(ref)
+        self.sections_combo.setGeometry(40, 35, 200, 35)
+
+        lbl2 = QLabel("Choose needed format", self)
+        lbl2.setGeometry(40, 70, 200, 35)
+
+        self.format_combo = QComboBox(self)
+        for form in _FORMAT:
+            self.format_combo.addItem(form)
+        self.format_combo.setGeometry(40, 105, 200, 35)
+
+        self.sections_combo.activated[str].connect(self.sections_combo_Activated)
+        self.format_combo.activated[str].connect(self.format_combo_Activated)
+
+        btn1 = QPushButton("Button 1", self)
+        btn1.setGeometry(40, 140, 200, 35)
+
+        btn1.clicked.connect(self.buttonClicked)
+
+        self.setGeometry(300, 300, 400, 200)
+        self.setWindowTitle('QComboBox')
+        self.show()
+
+    def buttonClicked(self):
+        #Parser(self.href)
+        if self.window2 is None:
+            self.window2 = OutputWindow()
+        self.window2.show()
+        self.close()
+
+    def sections_combo_Activated(self, text):
+        self.href = _SECTIONS_REF.get(text)
+
+    def format_combo_Activated(self, text):
+        self.format = text
+
+
+class OutputWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.create_output_window()
+
+    def create_output_window(self):
+
+        self.output = QTextEdit(self)
+        self.output.move(150, 25)
+        self.output.setReadOnly(True)
+        self.output.setGeometry(10, 10, 695, 395)
+        self.output.setLineWrapMode(QTextEdit.NoWrap);
+
+        btn1 = QPushButton("Button 1", self)
+        btn1.setGeometry(40, 140, 200, 35)
+
+        btn1.clicked.connect(self.buttonClicked)
+
+        self.setGeometry(300, 300, 700, 400)
+        self.setWindowTitle('QComboBox')
+        self.show()
+
+    def buttonClicked(self):
+        self.close()
+
+    def print_in(self, string):
+        string += '\n'
+        self.output.insertPlainText(string)
+
 if __name__ == "__main__":
-    pars = Parser()
-    pars.parse_site('/top/')
+
+    app = QApplication(sys.argv)
+    ex = MenuWindow()
+    sys.exit(app.exec_())
+
+    #site_refer = ""
+    #pars = Parser(site_refer)
 
